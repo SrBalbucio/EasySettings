e8d71a06063c06e21f45703129222254ce090e41
gynt <gynt@users.noreply.github.com>
2017-08-05 23:24:52 +0200

    added two convenience functions

--- src/com/gynt/easysettings/implementation/SimpleConfig.java
+++ src/com/gynt/easysettings/implementation/SimpleConfig.java
@@ -1,8 +1,43 @@
 package com.gynt.easysettings.implementation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gynt.easysettings.api.Config;
 import com.gynt.easysettings.api.Directory;
+import com.gynt.easysettings.api.Section;
+import com.gynt.easysettings.api.Setting;
 
 public class SimpleConfig extends Mapper<Directory> implements Config {
+
+	@Override
+	public Setting getByHierarchyKey(String key) {
+		String[] keys = key.split("\\.");
+		Directory d = get(keys[0]);
+		if(d!=null) {
+			Section s = d.get(keys[1]);
+			if(s!=null) {
+				Setting se = s.get(keys[2]);
+				return se;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<String> getKeyHierarchy() {
+		ArrayList<String> result = new ArrayList<>();
+		for(Directory d : get()) {
+			String r = d.getKey();
+			for(Section s : d.get()) {
+				String re = r+"."+s.getKey();
+				for(Setting ss : s.get()) {
+					String res = re+"."+ss.getKey();
+					result.add(res);
+				}
+			}
+		}
+		return result;
+	}
 
 }
